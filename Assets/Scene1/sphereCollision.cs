7ae3e936bd216dbeeb811df164e161bbfd816dea
DeveloperBhin <norbeth559@gmail.com>
2026-01-05 10:38:44 +0300

    building

--- Assets/Scene1/sphereCollision.cs
+++ Assets/Scene1/sphereCollision.cs
@@ -1,12 +1,25 @@
-
 using UnityEngine;
+using UnityEngine.SceneManagement; 
 
 public class sphereCollision : MonoBehaviour
 {
-public SphereBehavior movement;
-   void OnCollisionEnter (Collision collisionInfo){
-   if(collisionInfo.collider.tag == "Obstacle"){
-movement.enabled = false;
-   }
-   }
+    public SphereBehavior movement;
+    public GameObject gameOverText;
+    public GameObject restartButton;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.CompareTag("Obstacle"))
+        {
+            movement.enabled = false;
+            gameOverText.SetActive(true);
+            restartButton.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 }
+
